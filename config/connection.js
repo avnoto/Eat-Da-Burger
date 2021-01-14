88ba5dad964f63a4bb55f51311102d6f474e1c74
avnoto <alexandravnoto@gmail.com>
2021-01-14 09:18:51 -0600

    typo in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  conneciton = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db',
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db",
   });
 }
 
